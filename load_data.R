9b81b412149c9e5515e3d8cd71029c7bd522c22a
Remi <90064274+Remi-Neal@users.noreply.github.com>
2026-04-02 10:59:00 -0600

    started analysis. need to create a taxonimizr style database in order to run metascope_id function

--- load_data.R
+++ load_data.R
@@ -2,38 +2,55 @@ bt_indices_metascope_ncbi <- function(){
   # sequences sourced from:
   # https://www.ncbi.nlm.nih.gov/nuccore/?term=((33175%5BBioProject%5D+OR+33317%5BBioProject%5D))
   message("Using pregenerated ncbi database")
-  return(file.path(getwd(), "data", "ncbi", "sequences.fasta"))
+  ncbi_data_dir <- file.path(getwd(), "data", "ncbi")
+  ncbi_bowtie_dir <- file.path(ncbi_data_dir, "indices")
+  return(ncbi_bowtie_dir)
 }
 
 bt_indices_metascope_silva <- function(){
   # load data from using MetaScope's download function
   # https://rutgers.app.box.com/s/njq1qc9bzawzs3a6513uihxxwgxu9qc5
-  ind_dir = file.path(getwd(),"data","silva")
-  dir.create(ind_dir)
-  taxonomy_name = "all_silva_headers.rds"
-  MetaScope::download_accessions(
-    ind_dir = ind_dir,
-    tmp_dir = file.path(ind_dir, "tmp"),
-    remove_tmp_dir = TRUE, 
-    NCBI_accessions_database = FALSE,
-    silva_taxonomy_database = TRUE,
-    silva_taxonomy_name = taxonomy_name
-  )
-  return(file.path(ind_dir,taxonomy_name))
+  silva_dir = file.path(getwd(),"data","silva")
+  silva_bowtie_dir <- file.path(silva_dir, "indices")
+  return(silva_bowtie_dir)
 }
 
 bt_indices_radlib_16S <- function(){
-  return(1)
+  rad_data_dir <- file.path(getwd(), "data", "rad")
+  rad16s_bowtie_dir <- file.path(rad_data_dir, "16s-indices")
+  rad16s_data_dir <- file.path(rad_data_dir, "16s")
+  dir.create(rad16s_bowtie_dir)
+  mk_bowtie_index(
+    ref_dir = rad16s_data_dir,
+    lib_dir = rad16s_bowtie_dir,
+    lib_name = "indices",
+    overwrite = TRUE
+  )
+  return(rad16s_bowtie_dir)
 }
 
 bt_indices_radlib_vr <- function(){
-  return(1)
+  rad_data_dir <- file.path(getwd(), "data", "rad")
+  radvr_bowtie_dir <- file.path(rad_data_dir, "vr-indices")
+  radvr_data_dir <- file.path(rad_data_dir, "vr")
+  dir.create(radvr_bowtie_dir)
+  mk_bowtie_index(
+    ref_dir = radvr_data_dir,
+    lib_dir = radvr_bowtie_dir,
+    lib_name = "target",
+    overwrite = TRUE
+  )
+  return(radvr_bowtie_dir)
 }
 
 load_dada2_test_data <- function(){
   # Load DADA2 tutorial test data https://benjjneb.github.io/dada2/tutorial.html
+  dada2_fastq_dir <- file.path(getwd(), "data", "test-data", "MiSeq_SOP")
+  return(dada2_fastq_dir)
 }
 
 load_real_world_test_data <- function(){
   # Load data sources from zenodo https://doi.org/10.5281/zenodo.18944121
+  v3v4_fastq_dir <- file.path(getwd(), "data", "test-data", "raw_fastq")
+  return(v3v4_fastq_dir)
 }
